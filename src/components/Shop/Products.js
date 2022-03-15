51e5f4fe6a7b5aea83fe4c13af87df8e29a7f4e3
boisset-c <boissetcosme@gmail.com>
2022-03-15 12:55:05 -0500

    Added cart total products functionality

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 3,
+    title: "my first product",
+    description: "my first description",
+  },
+  {
+    id: "p2",
+    price: 9,
+    title: "my second product",
+    description: "my second description",
+  },
+  {
+    id: "p3",
+    price: 5,
+    title: "my third product",
+    description: "my third description",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
